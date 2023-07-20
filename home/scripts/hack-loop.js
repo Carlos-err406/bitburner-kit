db548ae87a8cb933e44e06559a8ee11a2d589f84
Carlos Daniel Vilaseca <carlosd.vilaseca@generalsoftwareinc.com>
2023-07-20 14:37:12 -0400

    fix: not awaiting more than its needed

--- home/scripts/hack-loop.js
+++ home/scripts/hack-loop.js
@@ -3,12 +3,7 @@ const { hack } = HackTreeInitConfig
 /** @param {NS} ns */
 export async function main(ns) {
     const target = ns.args[0]
-    const min = 1
-    const max = 6
-
     while (true) {
-        const timeToWait = Math.floor(Math.random() * (max - min + 1) + min)
-        await ns.sleep(timeToWait * 1000)
         await ns.hack(target, { stock: hack.stock })
     }
 }
